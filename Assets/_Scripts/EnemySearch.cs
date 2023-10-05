15349ebad94cfce389d22a0039a219c27696d27e
Alexander Evstratov <alexdrugev@me.com>
2023-10-05 21:32:50 +0300

    Add enemy name and enemy image.

--- Assets/_Scripts/EnemySearch.cs
+++ Assets/_Scripts/EnemySearch.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
 using Newtonsoft.Json;
-using System.Xml.Linq;
 
 public class Root
 {
@@ -34,14 +34,18 @@ public class Picture
 
 public class EnemySearch : MonoBehaviour
 {
+    [SerializeField] private GameObject _loadWindow;
     [SerializeField] private TMP_Text _playerName;
+    [SerializeField] private TMP_Text _enemyName;
+    [SerializeField] private Image _enemyImage;
+
     private string _url = "https://randomuser.me/api/";
 
     private void Start()
     {
         string playerName = PlayerPrefs.GetString("PlayerName", "");
         _playerName.text = playerName;
-        StartCoroutine(SendRequest());
+        SearchButton();
     }
 
     public void FightButton()
@@ -51,14 +55,29 @@ public class EnemySearch : MonoBehaviour
 
     private IEnumerator SendRequest()
     {
+        _loadWindow.SetActive(true);
         UnityWebRequest request = UnityWebRequest.Get(_url);
         yield return request.SendWebRequest();
         string myJsonResponse = request.downloadHandler.text;
         Debug.Log(myJsonResponse);
         Debug.Log("-----------------------------------------------------------------------------");
-
         Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
-        Debug.Log(myDeserializedClass.results[0].name.first);
-        Debug.Log(myDeserializedClass.results[0].picture.medium);
+        string enemyName = myDeserializedClass.results[0].name.first;
+        _enemyName.text = enemyName;
+        string enemyImage = myDeserializedClass.results[0].picture.medium;
+        //Debug.Log(myDeserializedClass.results[0].name.first);
+        //Debug.Log(myDeserializedClass.results[0].picture.medium);
+
+        UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(enemyImage);
+        yield return webRequest.SendWebRequest();
+        Texture texture = ((DownloadHandlerTexture)webRequest.downloadHandler).texture;
+        _enemyImage.sprite = Sprite.Create((Texture2D)texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+
+        _loadWindow.SetActive(false);
+    }
+
+    public void SearchButton()
+    {
+        StartCoroutine(SendRequest());
     }
 }
